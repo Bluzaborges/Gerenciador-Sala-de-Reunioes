36cec4c7a66cf3fddca29e38fcee7e54e62f772d
Bruno Borges Luza <bluzaborges@hotmail.com>
2021-08-31 19:42:10 -0300

    Initial commit

---
+++ src/main/java/com/bluzaborges/saladereuniao/saladereuniao/model/Room.java
@@ -0,0 +1,4 @@
+package com.bluzaborges.saladereuniao.saladereuniao.model;
+
+public class Room {
+}
---
+++ src/test/java/com/bluzaborges/saladereuniao/saladereuniao/SalaDeReuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluzaborges.saladereuniao.saladereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaDeReuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
